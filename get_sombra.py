b9dea9548536a0a0423a8baf71081173c4300b0b
Elizabeth Williams <elizabeth.williams@ucf.edu>
2016-09-08 12:33:31 -0400

    Removed selenium

--- get_sombra.py
+++ get_sombra.py
@@ -1,32 +1,34 @@
-from selenium import webdriver
 from settings import SLACK_URL
 import requests
 import json
 import re
 
-driver = webdriver.Firefox()
+
+def update_slack():
+    if not SLACK_URL:
+        print "No slack_url"
+    else:
+        # format sombra text?
+        payload = {
+            "channel": "#overwatch",
+            "username": "Sombra",
+            "text": sombra_text,
+            "icon_emoji": ":skull:"
+        }
+        requests.post(SLACK_URL, data=json.dumps(payload))
+
 sombra_text = None
 try:
-    driver.get('http://amomentincrime.com/')
-    result = driver.find_element_by_css_selector('body > p > font').text
-    result = re.split("%", result)
-    result = re.split("... ", result[0])
-    sombra_text = result[-1]+"%"
-    print sombra_text
+    result = requests.get('http://amomentincrime.com/')
+    if result.status_code == 200:
+        # cool
+        result = re.split("%", result.text)
+        result = re.split("... ", result[0])
+        sombra_text = result[-1]+"%"
+        update_slack()
+
+    else:
+        sombra_text = ":warning: ?que no respondio a 200? :warning:"
+
 except:
-    sombra_text = "yo quebre :/"
-    print sombra_text
-
-driver.close()
-
-if not SLACK_URL:
-    print "No slack_url"
-else:
-    # format sombra text?
-    payload = {
-        "channel": "#overwatch",
-        "username": "Sombra",
-        "text": sombra_text,
-        "icon_emoji": ":skull:"
-    }
-    requests.post(SLACK_URL, data=json.dumps(payload))
+    sombra_text = ":warning: yo quebre :/ :warning:"
